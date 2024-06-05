4e6797229fbbc5465acd996db981589b30c22a1e
davidnyiringabo <nyiringabodavid62@gmail.com>
2024-06-05 09:15:50 +0200

    [format]: format codes

--- src/components/Inputs/AnimatedInput.tsx
+++ src/components/Inputs/AnimatedInput.tsx
@@ -1,8 +1,22 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useState } from 'react';
-import { BsEye, BsEyeSlash } from 'react-icons/bs';
+import React, { useState } from "react";
+import { BsEye, BsEyeSlash } from "react-icons/bs";
 
-const AnimatedInput = ({type, label, handleChange, value, className, showEye}: {type: string, label: string, handleChange: React.ChangeEventHandler<HTMLElement>, value: string, className?: string, showEye?:boolean}) => {
+const AnimatedInput = ({
+  type,
+  label,
+  handleChange,
+  value,
+  className,
+  showEye,
+}: {
+  type: string;
+  label: string;
+  handleChange: React.ChangeEventHandler<HTMLElement>;
+  value: string;
+  className?: string;
+  showEye?: boolean;
+}) => {
   const [isFocused, setIsFocused] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
 
@@ -11,14 +25,14 @@ const AnimatedInput = ({type, label, handleChange, value, className, showEye}: {
   };
 
   const handleBlur = () => {
-    if (value === '') {
+    if (value === "") {
       setIsFocused(false);
     }
   };
 
-//   const handleChange = (e: any) => {
-//     setInputValue(e.target.value);
-//   };
+  //   const handleChange = (e: any) => {
+  //     setInputValue(e.target.value);
+  //   };
 
   return (
     <div className="w-full flex flex-col items-start gap-2 relative">
@@ -26,8 +40,8 @@ const AnimatedInput = ({type, label, handleChange, value, className, showEye}: {
         htmlFor={label}
         className={`absolute left-3 top-3 transition-transform duration-200 ${
           isFocused || value
-            ? 'transform -translate-y-6 scale-75 z-50 bg-white text-[#1C1B1F] font-medium'
-            : 'transform translate-y-0 scale-100'
+            ? "transform -translate-y-6 scale-75 z-50 bg-white text-[#1C1B1F] font-medium"
+            : "transform translate-y-0 scale-100"
         }`}
       >
         {label}
@@ -41,12 +55,19 @@ const AnimatedInput = ({type, label, handleChange, value, className, showEye}: {
         onChange={handleChange}
         className={`${className} p-3 py-4 w-full border border-[#79747E] rounded-md text-sm text-[#1C1B1F] focus:outline-none focus:border-[#79747E]`}
       />
-      {(showEye) && (
-        <div className='absolute top-5 right-3 cursor-pointer'>
-            {showPassword ? (
-                <BsEyeSlash color='black' onClick={()=> setShowPassword(!showPassword)}/>
-            ) : 
-            <BsEye color='black' onClick={()=> setShowPassword(!showPassword)}/>}
+      {showEye && (
+        <div className="absolute top-5 right-3 cursor-pointer">
+          {showPassword ? (
+            <BsEyeSlash
+              color="black"
+              onClick={() => setShowPassword(!showPassword)}
+            />
+          ) : (
+            <BsEye
+              color="black"
+              onClick={() => setShowPassword(!showPassword)}
+            />
+          )}
         </div>
       )}
     </div>
